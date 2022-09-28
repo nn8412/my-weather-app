fc0c026c29e3a2f64b587391c7c8e0f4ca903e54
nn8412 <ninanketiah@gmail.com>
2022-09-27 23:48:09 -0400

    Removed console log from getDailyForecast function

--- src/index.js
+++ src/index.js
@@ -104,7 +104,6 @@ function showForecast(response) {
 }
 
 function getDailyForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = `7746bdeabca928cfedcad71e52fd9d66`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
